cda4be02a83eec56aef121a41d782dd67231b9f4
RODI-099 <rodiiy099@gmail.com>
2025-06-12 20:36:19 +0900

    Configure Pexels domain for Next.js images

--- next.config.mjs
+++ next.config.mjs
@@ -8,6 +8,15 @@ const nextConfig = {
   },
   images: {
     unoptimized: true,
+    domains: ['images.pexels.com'],
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'images.pexels.com',
+        port: '',
+        pathname: '/photos/**',
+      },
+    ],
   },
 }
 
