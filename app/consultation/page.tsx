b01939fd9aecf6beae89a981c53892f06b0da0e0
RODI-099 <rodiiy099@gmail.com>
2025-06-18 14:03:41 +0900

    Fix Next.js Invariant Error

--- app/consultation/page.tsx
+++ app/consultation/page.tsx
@@ -1,12 +1,4 @@
-"use client"
-
-import dynamic from 'next/dynamic'
-
-// Dynamically import the client component with SSR disabled
-const ConsultationClient = dynamic(
-  () => import('./consultation-client').then(mod => mod.ConsultationClient),
-  { ssr: false }
-)
+import { ConsultationClient } from "./consultation-client"
 
 export default function ConsultationPage() {
   return <ConsultationClient />
--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,12 +1,4 @@
-"use client"
-
-import dynamic from 'next/dynamic'
-
-// Dynamically import the client component with SSR disabled
-const DashboardClient = dynamic(
-  () => import('./dashboard-client').then(mod => mod.DashboardClient),
-  { ssr: false }
-)
+import { DashboardClient } from "./dashboard-client"
 
 export default function DashboardPage() {
   return <DashboardClient />
