e9d1622a2aac36b02b4d78175f645cbfa02b0965
RODI-099 <rodiiy099@gmail.com>
2025-06-30 10:21:12 +0900

    Fix SQL Syntax Error

---
+++ supabase/migrations/20250630012101_bright_cottage.sql
@@ -0,0 +1,5 @@
+-- The original query has a syntax error:
+-- SELECT * FROM my_table WHERE my_column = 'some value;
+
+-- Here's the corrected query with proper quote closing:
+SELECT * FROM my_table WHERE my_column = 'some value';
